477c2fbeae660a3e815c29c4f3eba8b2a3fe5ff9
Ahmed Elwan <34627356+A7mad3lwan@users.noreply.github.com>
2024-05-28 14:16:46 +0300

    splash view done

--- lib/view/screens/splash_view.dart
+++ lib/view/screens/splash_view.dart
@@ -1,3 +1,5 @@
+import 'package:e_commerce/core/app%20theme/font_style.dart';
+import 'package:e_commerce/core/constants/assets.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatefulWidget {
@@ -7,11 +9,54 @@ class SplashView extends StatefulWidget {
   State<SplashView> createState() => _SplashViewState();
 }
 
-class _SplashViewState extends State<SplashView> {
+class _SplashViewState extends State<SplashView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingText;
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 750));
+    slidingText = Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero)
+        .animate(animationController);
+    animationController.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Scaffold(),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Image.asset(
+                Assets.logo,
+                fit: BoxFit.fill,
+              ),
+            ),
+            const SizedBox(height: 15),
+            AnimatedBuilder(
+              animation: animationController,
+              builder: (context, child) => SlideTransition(
+                position: slidingText,
+                child: const Text(
+                  'welcome to e-commerce app',
+                  style: FontStyles.font18,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
