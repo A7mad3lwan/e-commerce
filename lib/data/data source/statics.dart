1d4d94c61e2ed864d04c9d253c276a7051638b9f
Ahmed Elwan <34627356+A7mad3lwan@users.noreply.github.com>
2024-05-28 14:53:33 +0300

    create onboarding model and statics

---
+++ lib/data/data source/statics.dart
@@ -0,0 +1,22 @@
+import 'package:e_commerce/core/constants/assets.dart';
+import 'package:get/get.dart';
+
+import '../model/onboarding/onboarding_model.dart';
+
+List<OnboardingModel> onboardingList = [
+  OnboardingModel(
+    title: '2'.tr,
+    body: '3'.tr,
+    image: Assets.onboarding1,
+  ),
+  OnboardingModel(
+    title: '4'.tr,
+    body: '5'.tr,
+    image: Assets.onboarding2,
+  ),
+  OnboardingModel(
+    title: '6'.tr,
+    body: '7'.tr,
+    image: Assets.onboarding3,
+  ),
+];
