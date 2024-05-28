fccb68d341596253498539cdd26c49d91dcb43a9
Ahmed Elwan <34627356+A7mad3lwan@users.noreply.github.com>
2024-05-28 14:06:39 +0300

    add assets

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,6 @@
+class Assets {
+  static const logo = 'assets/images/logo.png';
+  static const onboarding1 = 'assets/images/onboarding1.png';
+  static const onboarding2 = 'assets/images/onboarding2.png';
+  static const onboarding3 = 'assets/images/onboarding3.png';
+}
