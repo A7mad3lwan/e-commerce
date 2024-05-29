8f473d3dec9037492a026af4ed0a3a9cef7c9966
Ahmed Elwan <34627356+A7mad3lwan@users.noreply.github.com>
2024-05-29 03:42:18 +0300

    create signup controller

---
+++ lib/core/controller/auth/signup_controller.dart
@@ -0,0 +1,71 @@
+import 'package:e_commerce/core/app%20router/app_routes.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+
+abstract class SignupController extends GetxController {
+  signup();
+
+  toLogin();
+}
+
+class SignupControllerImpl extends SignupController {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  late TextEditingController username;
+  late TextEditingController phone;
+  late TextEditingController email;
+  late TextEditingController password;
+
+  bool? obscure = true;
+  IconData suffixIcon = FontAwesomeIcons.eye;
+
+  showPassword() {
+    if (obscure = obscure == true) {
+      obscure = false;
+      suffixIcon = FontAwesomeIcons.eyeSlash;
+      update();
+    } else {
+      obscure = true;
+      suffixIcon = FontAwesomeIcons.eye;
+      update();
+    }
+  }
+
+  @override
+  void onInit() {
+    username = TextEditingController();
+    phone = TextEditingController();
+    email = TextEditingController();
+    password = TextEditingController();
+    super.onInit();
+  }
+
+  @override
+  signup() {
+    var formData = formKey.currentState;
+    if (formData!.validate()) {
+      if (kDebugMode) {
+        print('valid data!');
+      }
+    } else {
+      if (kDebugMode) {
+        print('valid data!');
+      }
+    }
+  }
+
+  @override
+  toLogin() {
+    Get.offAllNamed(AppRoutes.login);
+  }
+
+  @override
+  void dispose() {
+    username.dispose();
+    phone.dispose();
+    email.dispose();
+    password.dispose();
+    super.dispose();
+  }
+}
--- lib/view/screens/auth/signup_view_body.dart
+++ lib/view/screens/auth/signup_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce/core/constants/custom_form_field.dart';
+import 'package:e_commerce/core/functions/validator.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
@@ -17,21 +18,34 @@ class SignupViewBody extends StatelessWidget {
       children: [
         const SignupHeader(),
         CustomFormField(
+          validator: (val) {
+            return appValidator(val!, 'username', 2, 100);
+          },
           labelText: '23'.tr,
           hintText: '24'.tr,
           suffix: FontAwesomeIcons.user,
         ),
         CustomFormField(
+          validator: (val) {
+            return appValidator(val!, 'phone', 10, 100);
+          },
+          keyboardType: TextInputType.phone,
           labelText: '25'.tr,
           hintText: '26'.tr,
           suffix: FontAwesomeIcons.phone,
         ),
         CustomFormField(
+          validator: (val) {
+            return appValidator(val!, 'email', 10, 100);
+          },
           labelText: '14'.tr,
           hintText: '15'.tr,
           suffix: FontAwesomeIcons.envelope,
         ),
         CustomFormField(
+          validator: (val) {
+            return appValidator(val!, 'password', 2, 8);
+          },
           labelText: '16'.tr,
           hintText: '17'.tr,
           suffix: FontAwesomeIcons.eye,
